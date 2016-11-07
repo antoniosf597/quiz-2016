7d7a7c3775e2e834cf8726648f247366a551afcc
Antonio Sanchez <antoniosf597@gmail.com>
2016-11-07 09:02:12 +0100

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controllers');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz-2016' });
 });
+
+// Autoload
+router.param('quizId', quizController.load); // autoload :quizId
 
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer',quizController.answer);
