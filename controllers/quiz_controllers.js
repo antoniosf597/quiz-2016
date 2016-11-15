0724f64c3bcbca3243a0717ea19cd0af78777fff
Antonio Sanchez <antoniosf597@gmail.com>
2016-11-15 09:17:03 +0100

    Editar y borrar

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -38,6 +38,12 @@ exports.show = function(req,res){
 	
 
 };
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then( function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
 
 //GET /quizes/:id/answer
 exports.answer = function(req,res){
@@ -57,10 +63,42 @@ exports.answer = function(req,res){
 // GET /quizes/new
 exports.new = function(req, res){
 	var quiz = models.Quiz.build( // crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		
 		);
 		res.render('quizes/new', {quiz: quiz});
 	};
+
+// GET quizes/:id/edit
+
+exports.edit = function(req, res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+req.quiz
+.validate()
+.then(
+	function(err){
+		if(err){
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}else{
+			req.quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then(function(){res.redirect('/quizes');});
+
+			}
+		}
+	);
+	};
+
+
+
+
 
 // POST /quizes/create
 
