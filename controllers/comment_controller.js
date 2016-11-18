aa21a709d0b2e3a657e77c06923c9c6510341118
Antonio Sanchez <antoniosf597@gmail.com>
2016-11-18 12:19:04 +0100

    Comentarios definitivo

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -19,7 +19,7 @@ exports.create = function(req, res){
 	.then(
 		function(err){
 			if(err){
-				res.render('comment/new.ejs',
+				res.render('comments/new.ejs',
 					{comment: comment, quizid: req.params.quizId, errors: err.errors});
 
 			}else{
